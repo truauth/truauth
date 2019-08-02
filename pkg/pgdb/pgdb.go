17f41774fe1fb496510898f80af475571cbd31ba
GuyARoss <guyallenross@gmail.com>
2019-08-02 16:20:32 -0400

    pgdb cleanup moved out settings init to builder.

--- pkg/pgdb/pgdb.go
+++ pkg/pgdb/pgdb.go
@@ -2,18 +2,18 @@ package pgdb
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
+
 	_ "github.com/lib/pq" // comment justifing it.. lol
-	"os"
+	"github.com/truauth/truauth/pkg/settings"
 )
 
 // DbCreds struct for holding db creds
 type DbCreds struct {
-	Host 		string
-	User 		string
-	Password 	string
-	Dbname		string
+	Host     string
+	User     string
+	Password string
+	Dbname   string
 }
 
 // CreateSession creates a pdb session
@@ -31,17 +31,9 @@ func CreateSession(creds *DbCreds) *sql.DB {
 
 // FetchCreds fetches the db credentials from local en
 func FetchCreds() *DbCreds {
-	file, _ := os.Open("./postgres_env.json")
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-
 	creds := &DbCreds{}
-	err := decoder.Decode(creds)
 
-	if err != nil {
-		return nil
-	}
+	settings.Init(creds, "postgres_env")
 
 	return creds
 }
