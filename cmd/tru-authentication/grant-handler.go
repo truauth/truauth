97b38c42cb862cb5820dc135ec3c7e0d9748f429
GuyARoss <guyallenross@gmail.com>
2019-08-02 14:35:58 -0400

    ported grant handler.

---
+++ cmd/tru-authentication/grant-handler.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	lib "github.com/truauth/truauth/pkg/auth-lib"
+)
+
+// HandleGrantType grant type handler
+func (req *Request) HandleGrantType(request *lib.AuthTokenRequest) (*lib.AccessTokenResponse, *lib.AuthErrorResponse) {
+	if request.GrantType == "authorization_code" {
+		return req.handleGrantAuthorizationCode(request)
+	} else if request.GrantType == "refresh_token" {
+		return req.handleGrantRefreshToken(request)
+	}
+
+	return nil, lib.CreateError(lib.UnauthorizedClient, "invalid access grant")
+}
+
+func (req *Request) handleGrantAuthorizationCode(request *lib.AuthTokenRequest) (*lib.AccessTokenResponse, *lib.AuthErrorResponse) {
+	decodedCodeToken := lib.DecodeCodeJWT(req.Environment.JWTSecret, request.Code)
+
+	authError := request.Validate(decodedCodeToken)
+	if authError != nil {
+		return nil, authError
+	}
+
+	// ~ access token creation
+	duration := req.Configuration.ExpireDuration
+	accessToken := lib.CreateAccessToken(request.ClientID, decodedCodeToken.UserID, duration, decodedCodeToken.Scope)
+	signedToken := accessToken.ToJWT(request.ClientSecret)
+
+	// refresh token creation
+	refreshToken := lib.CreateRefreshToken(request.ClientID, decodedCodeToken.UserID, decodedCodeToken.Scope)
+	signedRefreshToken := refreshToken.ToJWT(request.ClientSecret)
+
+	return lib.CreateAccessTokenResponse(signedToken, lib.BearerToken, duration, signedRefreshToken), nil
+}
+
+func (req *Request) handleGrantRefreshToken(request *lib.AuthTokenRequest) (*lib.AccessTokenResponse, *lib.AuthErrorResponse) {
+	if request.RefreshToken == "" {
+		lib.CreateError(lib.InvalidRequest, "refresh token is requried for the refresh token grant")
+	}
+
+	refreshToken := lib.DecodeSignedRefreshToken(request.ClientSecret, request.RefreshToken)
+	if refreshToken == nil {
+		lib.CreateError(lib.InvalidToken, "invalid token")
+	}
+
+	// ~ access token creation
+	duration := req.Configuration.ExpireDuration
+	accessToken := lib.CreateAccessToken(request.ClientID, refreshToken.ClientID, duration, refreshToken.Scope)
+	signedToken := accessToken.ToJWT(request.ClientSecret)
+
+	return lib.CreateAccessTokenResponse(signedToken, lib.BearerToken, duration, request.RefreshToken), nil
+}
