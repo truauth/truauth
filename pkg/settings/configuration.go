58b1e05d0f42ecb0cac7c93c4cf3b2915611d663
GuyARoss <guyallenross@gmail.com>
2019-08-07 09:52:28 -0400

    updated the configuration.

--- pkg/settings/configuration.go
+++ pkg/settings/configuration.go
@@ -2,6 +2,7 @@ package settings
 
 // Configuration configuration struct used to store safe application settings
 type Configuration struct {
-	ExpireDuration         int
-	IdentityServiceAddress string
+	ExpireDuration              int
+	IdentityServiceAddress      string
+	AuthorizationServiceAddress string
 }
