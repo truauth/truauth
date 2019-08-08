030d184aadc2367ccda559d594439772959cb722
GuyARoss <guyallenross@gmail.com>
2019-08-08 12:11:47 -0400

    added function to execute bytes.

--- pkg/web-serve/file-serve.go
+++ pkg/web-serve/file-serve.go
@@ -3,6 +3,7 @@ package webserve
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"text/template"
 
 	"github.com/truauth/truauth/pkg/minify"
@@ -12,6 +13,7 @@ import (
 func Init(fileNames ...string) Files {
 	files := Files{}
 
+	// todo: remove template brackets on bytes init.
 	for _, fileName := range fileNames {
 		path := fmt.Sprintf("../../static/%s.html", fileName)
 		data, err := ioutil.ReadFile(path)
@@ -43,4 +45,10 @@ func Init(fileNames ...string) Files {
 // RetrieveFile retrieves a byte array from memory
 func (files Files) RetrieveFile(name string) *File {
 	return files[name]
+}
+
+// ExecuteBytes exectutes the file bytes to the response writer
+func (file File) ExecuteBytes(w http.ResponseWriter) {
+	w.Write(*file.Data)
+	w.WriteHeader(http.StatusFound)
 }
