8fc8ad5dee632f50ffc285ce67b69ce01a0e808f
GuyARoss <guyallenross@gmail.com>
2019-08-08 12:08:36 -0400

    added keep alive params.

--- cmd/tru-authentication/grpcservices/authorization.go
+++ cmd/tru-authentication/grpcservices/authorization.go
@@ -1,7 +1,10 @@
 package grpcservices
 
 import (
+	"time"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 
 	grpcAuthorization "github.com/truauth/truauth/pkg/grpc-authorization"
 )
@@ -13,7 +16,12 @@ type AuthorizationClient struct {
 
 // CreateAuthorizationGRPCClient creates a grpc authorization client
 func CreateAuthorizationGRPCClient(hostAddress string) *AuthorizationClient {
-	cc, err := grpc.Dial(hostAddress, grpc.WithInsecure())
+	k := keepalive.ClientParameters{
+		Time:                time.Minute * 2,
+		PermitWithoutStream: true,
+	}
+
+	cc, err := grpc.Dial(hostAddress, grpc.WithInsecure(), grpc.WithKeepaliveParams(k))
 	if err != nil {
 		panic(err)
 	}
